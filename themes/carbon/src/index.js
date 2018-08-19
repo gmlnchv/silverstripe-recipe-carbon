a710cee203f95adc10b91d6832af6adc9cf8a47d
Georgy Malanichev <georgy@malanichev.com>
2018-08-19 21:35:21 +1200

    Add a few basic demo components to HomePage

--- themes/carbon/src/index.js
+++ themes/carbon/src/index.js
@@ -1,1 +1,34 @@
+import { Accordion, Modal } from 'carbon-components';
+const prefix = 'dn';
+
+const init = () => {
+  Accordion.init(document, {
+    selectorInit: '[data-accordion]',
+    selectorAccordionItem: '[data-accordion-item]',
+    selectorAccordionItemHeading: `.${prefix}--accordion__heading`,
+    selectorAccordionContent: `.${prefix}--accordion__content`,
+    classActive: `${prefix}--accordion__item--active`
+  });
+
+  Modal.init(document, {
+    selectorInit: '[data-modal]',
+    selectorModalClose: '[data-modal-close]',
+    selectorPrimaryFocus: '[data-modal-primary-focus]',
+    selectorsFloatingMenus: [
+      `.${prefix}--overflow-menu-options`,
+      `.${prefix}--tooltip`,
+      '.flatpickr-calendar'
+    ],
+    classVisible: 'is-visible',
+    attribInitTarget: 'data-modal-target',
+    initEventNames: ['click'],
+    eventBeforeShown: 'modal-beingshown',
+    eventAfterShown: 'modal-shown',
+    eventBeforeHidden: 'modal-beinghidden',
+    eventAfterHidden: 'modal-hidden'
+  });
+};
+
+document.addEventListener('DOMContentLoaded', init);
+
 import './scss/styles.scss';
---
+++ themes/carbon/templates/Layout/HomePage.ss
@@ -0,0 +1,6 @@
+<div class="dn--grid max">
+  <div class="page">
+    <% include Demo/Accordion %>
+    <% include Demo/Modal %>
+  </div>
+</div>

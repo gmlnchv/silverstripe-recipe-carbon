f42e9fe248c3427a7aa8753667de31b45cfe3faf
Georgy Malanichev <georgy@malanichev.com>
2018-08-30 22:45:25 +1200

    Add webpack loaders to optimise svgs and images

--- themes/carbon/webpack.common.js
+++ themes/carbon/webpack.common.js
@@ -18,21 +18,43 @@ module.exports = {
             options: {
               name: '[name].[ext]',
               outputPath: 'fonts/',
-              publicPath: 'resources/themes/carbon/dist/fonts'
+              publicPath: '/resources/themes/carbon/dist/fonts'
             }
           }
         ]
       },
       {
-        test: /\.(bmp|gif|jpg|jpeg|png|svg)$/,
+        // Inline lightweight SVGs as UTF-8 encoded DataUrl string
+        // The result is smaller and faster to parse than base64
+        test: /\.svg$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: 'svg-url-loader',
             options: {
+              limit: 10 * 1024,
               name: '[name].[ext]',
               outputPath: 'images/',
-              publicPath: 'resources/themes/carbon/dist/images'
+              publicPath: '/resources/themes/carbon/dist/images'
             }
+          }
+        ]
+      },
+      {
+        // Inline lightweight images as Base64 encoded DataUrl string
+        test: /\.(jpe?g|png|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10 * 1024,
+              name: '[name].[ext]',
+              outputPath: 'images/',
+              publicPath: '/resources/themes/carbon/dist/images'
+            }
+          },
+          // Pass images through optimisers
+          {
+            loader: 'image-webpack-loader'
           }
         ]
       }
